7f1922e91af8f018c22614c2f07d4abc89627980
Quang Duong <duongquang1@gmail.com>
2015-07-05 16:26:13 -0500

    Build blog.

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 cd C:\Users\Quang\repos\quangmire.github.io\build
 call jekyll build
 robocopy "C:\Users\Quang\repos\quangmire.github.io\build\_site" "C:\Users\Quang\repos\quangmire.github.io" /s
-bash.exe github.bash
+"C:\Program Files (x86)\Git\bin\sh.exe"
 pause

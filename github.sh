cd823e8279049afb3d755b42efae7faee36b1f73
Quang Duong <duongquang1@gmail.com>
2015-07-05 16:28:19 -0500

    Build blog.

--- github.sh
+++ github.sh
@@ -1,7 +1,7 @@
-cd C:\Users\Quang\repos\quangmire.github.io\build
+cd C:/Users/Quang/repos/quangmire.github.io/build
 call jekyll build
 robocopy "C:\Users\Quang\repos\quangmire.github.io\build\_site" "C:\Users\Quang\repos\quangmire.github.io" /s
-cd C:\Users\Quang\repos\quangmire.github.io
+cd C:/Users/Quang/repos/quangmire.github.io
 eval $(ssh-agent -s)
 git checkout
 git add -A
